115addc7e0315c4822f146b05e4a8e7bd2601b56
harold <ananbbnn@gmail.com>
2025-08-26 18:01:33 +0800

    '首頁完成依員工顯示'

--- Homepage.py
+++ Homepage.py
@@ -3,19 +3,66 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 from datetime import datetime
 import plotly.graph_objects as go
+import altair as alt
 import upload
 import select_sql
 
 
+def mainpage_container(df, employee):
+    df = df[df['員工'] == employee]
+    df = df.drop(columns=['員工'])
+    # 轉換成長格式
+    df_melt = df.melt(id_vars=["報告日期"], var_name="類別", value_name="數量")
+    selection = alt.selection_point(fields=["類別"], bind="legend")
 
-st.title("QA Report")
+    #print(df)
+    with st.container(border=1 ,height=300):
+        st.write(employee + ' 統計圖表')
+        chart = alt.Chart(df_melt).mark_line(point=True).encode(
+            x='monthdate(報告日期):O',  # Time data type for the x-axis
+            y=alt.Y("數量:Q", axis=alt.Axis(format="d", tickMinStep=1)), # Quantitative data type for the y-axis
+            color='類別:N', # Nominal data type for coloring by category
+            tooltip=["報告日期:T", "類別:N", "數量:Q"],
+            opacity=alt.condition(selection, alt.value(1), alt.value(0))  # 未選取時隱藏
+            ).transform_filter(
+            selection  # 只保留選到的類別
+            ).add_params(
+            selection
+            ).properties(height=215)
+
+        st.altair_chart(chart, use_container_width=True)
+
+def set_2columns(df,num1,num2):
+    col1, col2 = st.columns([1,1])
+    with col1:
+        mainpage_container(df, employee_list[num1])
+    with col2:
+        try:
+            mainpage_container(df, employee_list[num2])
+        except IndexError:
+            return
+
+st.set_page_config(page_title="QA Report", layout="wide")
 
 with st.sidebar:
     selected = option_menu("選單", ["首頁", "上傳CSV", "每日統計查詢", "每月統計圖表"])
 #首頁
 if selected == "首頁":
     st.subheader('首頁')
-    st.slider("這是一個範例滑桿", 0, 100, 50)
+    
+    tab1,tab2 = st.tabs(['依員工分類','依資料類別分類'])
+
+    with tab1:
+        df_30 = select_sql.search_last30days_result()
+        employee_list = select_sql.search_employee_list()
+        for i in range(0,len(employee_list),2):
+            set_2columns(df_30,i,i+1)
+
+    with tab2:
+        st.write("這是依資料類別分類的內容")
+
+        if st.button("查詢"):
+            st.dataframe(select_sql.search_last30days_result())
 
 
 #上傳CSV
@@ -75,7 +122,7 @@ if selected == "每日統計查詢":
             barmode='group',  # 群組顯示
             xaxis_title="員工",
             yaxis_title="數量",
-            title=f"{daily_results} 統計結果",
+            title=f"{daily_date} 統計結果",
             height=500
         )
 
@@ -89,27 +136,5 @@ if selected == "每日統計查詢":
 if selected == "每月統計圖表":
     st.subheader('每月統計圖表')
 
-    col1, col2, col3 = st.columns([2,2,1],vertical_alignment='bottom')
-    with col1:
-        # 年份選擇
-        years = list(range(2020, datetime.now().year + 1))
-        selected_year = st.selectbox("選擇年份", years, index=len(years)-1)
-
-    with col2:
-        # 月份選擇
-        months = list(range(1, 13))
-        selected_month = st.selectbox("選擇月份", months, index=datetime.now().month-1)
-
-    with col3:
-        # 按鈕
-        query = st.button("查詢")
     
-    st.write(f"你選擇的月份：{selected_year}-{selected_month:02d}")
-
-    if query:
-        employee_list = select_sql.search_employee_list()
-        monthly_results = select_sql.search_monthly_results(selected_year, selected_month)
-        if monthly_results is None:
-            st.warning("查無資料，請確認日期是否正確或是否已上傳當月資料")
-            st.stop()
     
